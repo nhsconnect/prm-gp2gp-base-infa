e012b58b477480381c656fb51a5e9b24a34a92a1
James Green <jagreen@thoughtworks.com>
2020-11-12 16:52:15 +0000

    PRMT-1262 Add repo for mesh to s3 syncronizer

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -8,4 +8,13 @@ resource "aws_ecr_repository" "mi_data_collector" {
   }
 
 }
+resource "aws_ecr_repository" "mesh_s3_syncronizer" {
+  name = "gp2gp/mesh-s3-syncronizer"
 
+  tags = {
+    Name        = "Mesh S3 Syncronizer"
+    CreatedBy   = var.repo_name
+    Team        = var.team
+  }
+
+}
