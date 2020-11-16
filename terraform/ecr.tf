18496929a8871ec5243f39759a741c8395d6a2dd
Helen Zhou <helen.zhou1@nhs.net>
2020-11-16 16:07:50 +0000

    PRMT-1262 [HZ/JG] Correct spelling of Synchronizer

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -8,11 +8,11 @@ resource "aws_ecr_repository" "mi_data_collector" {
   }
 
 }
-resource "aws_ecr_repository" "mesh_s3_syncronizer" {
-  name = "gp2gp/mesh-s3-syncronizer"
+resource "aws_ecr_repository" "mesh_s3_synchronizer" {
+  name = "gp2gp/mesh-s3-synchronizer"
 
   tags = {
-    Name        = "Mesh S3 Syncronizer"
+    Name        = "Mesh S3 Synchronizer"
     CreatedBy   = var.repo_name
     Team        = var.team
   }
