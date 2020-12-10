6b33424e8d3cfdb2ae659a84e5a713211255ea23
Helen Zhou <helen.zhou1@nhs.net>
2020-12-10 10:31:44 +0000

    PRMT-1349 [HZ/JG] Remove redundent ECR repos

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,32 +1,3 @@
-resource "aws_ecr_repository" "mi_data_collector" {
-  name = "gp2gp/mi-data-collector"
-
-  tags = {
-    Name        = "MI Data Collector"
-    CreatedBy   = var.repo_name
-    Team        = var.team
-  }
-}
-resource "aws_ecr_repository" "mesh_s3_synchronizer" {
-  name = "gp2gp/mesh-s3-synchronizer"
-
-  tags = {
-    Name        = "Mesh S3 Synchronizer"
-    CreatedBy   = var.repo_name
-    Team        = var.team
-  }
-}
-
-resource "aws_ecr_repository" "mesh_file_s3_forwarder" {
-  name = "registrations/mesh-file-s3-forwarder"
-
-  tags = {
-    Name        = "Mesh File S3 Forwarder"
-    CreatedBy   = var.repo_name
-    Team        = var.team
-  }
-}
-
 resource "aws_ecr_repository" "mesh_inbox_s3_forwarder" {
   name = "registrations/mesh-inbox-s3-forwarder"
 
