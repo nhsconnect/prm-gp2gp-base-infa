c87e20a425d2eef0daa58077be1ab0f8bfecf087
Jake Newman <jacob.newman1@nhs.net>
2021-04-20 11:52:56 +0100

    PRMT-1795 [JN/JG] Destroy ods downloader from common and add .idea to .gitignore

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -7,14 +7,3 @@ resource "aws_ecr_repository" "mesh_inbox_s3_forwarder" {
     Team        = var.team
   }
 }
-
-resource "aws_ecr_repository" "ods_downloader" {
-  name = "registrations/ods-downloader"
-
-  tags = {
-    Name        = "Ods data downloader"
-    CreatedBy   = var.repo_name
-    Team        = var.team
-  }
-}
-
